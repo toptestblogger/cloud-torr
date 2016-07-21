ffded98616eeadec333e7e19aa73b322e1d58f79
toptestblogger <toptestblogger@gmail.com>
2016-07-21 21:51:14 +0530

    Update server_search.go

--- server/server_search.go
+++ server/server_search.go
@@ -4,8 +4,8 @@ package server
 //cloud-torrent uses "<id>-item" handlers
 var defaultSearchConfig = []byte(`{
 	"kat": {
-		"name": "Kickass Torrents",
-		"url": "https://kat.cr/usearch/{{query}}/{{page:1}}/?field=seeders&sorder=desc",
+		"name": "Extra Torrents",
+		"url": "https://extratorrent.cc/search/{{query}}/{{page:1}}/?field=seeders&sorder=desc",
 		"list": "#mainSearchTable table tr[id]",
 		"result": {
 			"name":".cellMainLink",
